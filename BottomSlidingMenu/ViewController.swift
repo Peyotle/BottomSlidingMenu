0dfc275ff046e43a119f8f993f5024c9c3b2a8e6
Oleg Chernyshenko <peyot3d@gmail.com>
2016-08-19 16:33:55 +1200

    Separated to classes

--- BottomSlidingMenu/ViewController.swift
+++ BottomSlidingMenu/ViewController.swift
@@ -8,65 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+protocol DraggableView {
+    weak var topContainerConstraint: NSLayoutConstraint! {get set}
+    func configure()
+}
 
-    @IBOutlet weak var topContainerConstraint: NSLayoutConstraint!
-    @IBOutlet weak var containerView: UIView!
+class DraggableMenuContainerView: UIView {
 
-    let positions:[CGFloat] = [30.0, 200.0, 500.0]
-    var dragging = false
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.configure()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.configure()
+    }
+
+    func configure() {
+        self.layer.cornerRadius = 10.0
+        self.clipsToBounds = false
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.masksToBounds = false
+        self.layer.shadowOpacity = 0.2
+
+        let shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: 10)
+        self.layer.shadowPath = shadowPath.cgPath
+    }
+}
+
+class DraggableMenuController: NSObject {
+
+    var topContainerConstraint: NSLayoutConstraint
+    var positions:[CGFloat] = [50]
+    var containingView: UIView
+    var slidingView: UIView
+
     var touchOffset: CGFloat = 0.0
+    var dragging = false
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    init(constraint: NSLayoutConstraint, slidingView: UIView, containingView: UIView) {
+        self.topContainerConstraint = constraint
+        self.slidingView = slidingView
+        self.containingView = containingView
+        super.init()
+
         let panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(self.panGestureHandler))
-        self.view.addGestureRecognizer(panRecognizer)
-
-        self.containerView.layer.cornerRadius = 10.0
-        self.containerView.clipsToBounds = false
-        self.containerView.layer.shadowColor = UIColor.black.cgColor
-        self.containerView.layer.masksToBounds = false
-        self.containerView.layer.shadowOpacity = 0.2
-
-        topContainerConstraint.constant = positions[0]
-    }
-
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        let shadowPath = UIBezierPath(roundedRect: self.containerView.bounds, cornerRadius: 10)
-        self.containerView.layer.shadowPath = shadowPath.cgPath
-    }
-
-    func panGestureHandler(recognizer: UIPanGestureRecognizer) {
-        let touch = recognizer.location(in: self.view)
-        switch recognizer.state {
-        case .began:
-            if touch.y >= topContainerConstraint.constant - 20 {
-                dragging = true
-                touchOffset = topContainerConstraint.constant - touch.y
-            }
-            break
-        case .changed:
-            if dragging {
-                topContainerConstraint.constant = touch.y + touchOffset
-            }
-            break
-        case .ended:
-            if dragging {
-                let velocity = recognizer.velocity(in: self.view).y
-                self.snapToClosestPosition(with: velocity)
-            }
-            dragging = false
-            break
-
-        default:
-            break
-        }
+        self.containingView.addGestureRecognizer(panRecognizer)
     }
 
     func snapToClosestPosition(with velocity: CGFloat = 0) {
         var closestPosition = positions[0]
-        var minDistance = self.view.frame.height
+        var minDistance: CGFloat = CGFloat(MAXFLOAT)
 
         for position in positions {
             let distance = abs(topContainerConstraint.constant - position + velocity/10)
@@ -87,9 +80,9 @@ class ViewController: UIViewController {
                        options: [],
                        animations: {
                         self.topContainerConstraint.constant = closestPosition
-                        self.view.layoutSubviews()
+                        self.containingView.layoutSubviews()
         }) { (completed) in
-            
+
         }
     }
 
@@ -98,11 +91,52 @@ class ViewController: UIViewController {
         return duration
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func panGestureHandler(recognizer: UIPanGestureRecognizer) {
+
+        let touch = recognizer.location(in: self.containingView)
+        switch recognizer.state {
+        case .began:
+            if touch.y >= topContainerConstraint.constant - 20 {
+                dragging = true
+                touchOffset = topContainerConstraint.constant - touch.y
+            }
+            break
+        case .changed:
+            if dragging {
+                topContainerConstraint.constant = touch.y + touchOffset
+            }
+            break
+        case .ended:
+            if dragging {
+                let velocity = recognizer.velocity(in: self.containingView).y
+                self.snapToClosestPosition(with: velocity)
+            }
+            dragging = false
+            break
+
+        default:
+            break
+        }
     }
-
-
 }
 
+class ViewController: UIViewController {
+
+    @IBOutlet weak var topContainerConstraint: NSLayoutConstraint!
+    @IBOutlet weak var containerView: UIView!
+
+    var draggableMenuController: DraggableMenuController?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.configureMenuController()
+    }
+
+    func configureMenuController() {
+        draggableMenuController = DraggableMenuController(constraint: self.topContainerConstraint, slidingView: self.containerView, containingView: self.view)
+        draggableMenuController?.containingView = self.view
+//        draggableMenuController?.slidingView = containerView
+        let positions: [CGFloat] = [30.0, 200.0, 500.0]
+        draggableMenuController?.positions = positions
+    }
+}
